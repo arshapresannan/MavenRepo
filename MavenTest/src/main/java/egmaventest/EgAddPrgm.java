7e7acb1b15ef2df3ee6ebe9c35c07a487905d0b0
arshapresannan <101731042+arshapresannan@users.noreply.github.com>
2022-03-18 12:30:41 +0530

    Uploading my project to github repository

---
+++ MavenTest/src/main/java/egmaventest/EgAddPrgm.java
@@ -0,0 +1,14 @@
+package egmaventest;
+
+public class EgAddPrgm {
+
+	public static void main(String[] args) {
+		
+		int a=50;
+		int b=30;
+		int c=a+b;
+		System.out.println("Result is :"+c);
+
+	}
+
+}
